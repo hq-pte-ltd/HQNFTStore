92eadb9397aff99beaaaafcbb8417e77b1fc9e42
jdojade <jade.delima.oloroso@gmail.com>
2022-07-03 13:16:19 +0800

    update fonts

--- js/packages/web/src/pages/_app.tsx
+++ js/packages/web/src/pages/_app.tsx
@@ -10,6 +10,10 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Homeqube NFT Marketplace</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&amp;display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <div id="root">
         <Component {...pageProps} />
